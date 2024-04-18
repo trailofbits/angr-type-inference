4ee58dfc5bb85da147c8caf865172a315167633b
2over12 <ian.smith@trailofbits.com>
2024-04-18 19:31:16 -0400

    eval fixes

--- angr/utils/merger_utils.py
+++ angr/utils/merger_utils.py
@@ -3,6 +3,8 @@ import pickle
 from .eval_binary_type_inference import ComparisonData
 import itertools
 from dataclasses import dataclass
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 @dataclass
@@ -18,6 +20,8 @@ def main():
     prser = argparse.ArgumentParser("merge_eval_files")
     prser.add_argument("file1")
     prser.add_argument("file2")
+    prser.add_argument("--out1", default=None, required=False, type=str)
+    prser.add_argument("--out2", default=None, required=False, type=str)
 
     args = prser.parse_args()
 
@@ -35,26 +39,51 @@ def main():
     with open(args.file2, "rb") as f:
         lst2: list[ComparisonData] = load_all(f)
 
-    shared_tgts = set()
-    for x in itertools.chain(lst1, lst2):
-        shared_tgts.add((x.binary_name, x.func_addr))
+    tgts_1 = set([(x.binary_name, x.func_addr) for x in lst1])
+    tgts_2 = set([(x.binary_name, x.func_addr) for x in lst2])
+    shared_tgts = set.intersection(tgts_1, tgts_2)
 
     def get_dist_total_and_average(lst: list[ComparisonData]) -> AggregateScores:
+        seen_set = set()
         fltered: list[ComparisonData] = [x for x in lst if (
             x.binary_name, x.func_addr) in shared_tgts]
+        for x in fltered:
+            assert (x.binary_name, x.func_addr) not in seen_set
+            seen_set.add((x.binary_name, x.func_addr))
+
+        print(np.std([x.type_distance for x in fltered]))
         tot_dist = sum(map(lambda x: x.type_distance, fltered))
         average_dist = tot_dist/len(fltered)
+        print(len(fltered))
         tot_time = sum(
             map(lambda x: x.ns_time_spent_during_inference, fltered))
         average_time = tot_time/len(fltered)
+        print(len(fltered))
 
         time_by_size = list(
             map(lambda x: (x.func_size, x.ns_time_spent_during_inference), fltered))
 
         return AggregateScores(tot_dist, average_dist, tot_time, average_time, time_by_size)
 
-    print(get_dist_total_and_average(lst1))
-    print(get_dist_total_and_average(lst2))
+    def plot_agg(agg, tgt):
+        plt.plot([x for (x, _) in agg.time_by_size],
+                 [y for (_, y) in agg.time_by_size], 'ro')
+        plt.xlabel("Number of Constraints")
+        plt.ylabel("Time (ns)")
+        plt.savefig(tgt, format="svg")
+    agg1 = get_dist_total_and_average(lst1)
+    if args.out1:
+        plot_agg(agg1, args.out1)
+
+    agg2 = get_dist_total_and_average(lst2)
+    if args.out2:
+        plot_agg(agg2, args.out2)
+
+    print("Tgt1 average diff: ", agg1.average_diff)
+    print("Tgt2 average diff: ", agg2.average_diff)
+
+    print("Tgt1 average time: ", agg1.average_time)
+    print("Tgt2 average time: ", agg2.average_time)
 
 
 if __name__ == "__main__":
